fba1478c6096e69baad3c4f659b63ea4e844c874
Dianakyz <3222777my@gmail.com>
2023-01-14 13:47:20 +0300

    first commit

---
+++ src/main/java/com/example/term3/Term3Application.java
@@ -0,0 +1,13 @@
+package com.example.term3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Term3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Term3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/term3/Term3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.term3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Term3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
